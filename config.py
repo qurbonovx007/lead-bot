664176bab4eb2512350bf857d1170b04d5ae3b11
qurbonov <qabdulhamid6@gmail.com>
2026-06-22 12:01:38 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 # Bot token - @BotFather dan oling
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8873850157:AAHtoOjzwVgicLLxDdcM-KlBX5IwsydbCuA")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8435237137:AAHPaHq_emn7cVpWe-7JLnfOOG_IM7zp0Mw")
 
 # Admin Telegram ID - @userinfobot dan oling
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "6454387575").split(",")))
 
 # Leads yuboriluvchi guruh/kanal ID (minus bilan, masalan: -1001234567890)
-LEADS_CHAT_ID = int(os.getenv("LEADS_CHAT_ID", "-5367270112"))
+LEADS_CHAT_ID = int(os.getenv("LEADS_CHAT_ID", "-5341403054"))
 
 # Bot haqida matn
 BOT_ABOUT = """
